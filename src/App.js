d2ac4ba399275b58fc14d73ba6ec4c57bffcbb80
Taesu Hyeon <qvil1127@gmail.com>
2018-10-16 00:17:07 +0900

    #1.3.1 Intermission for Bug Fixing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+// import { Query } from "react-apollo";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        {/* <Query query={GET_NOTES}>{() => null}</Query> // For Fix error */}
+        App
+      </div>
+    );
   }
 }
 
