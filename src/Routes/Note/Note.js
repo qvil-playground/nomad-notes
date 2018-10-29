e853242c5076aa42fa4ecb912f5a49ef650070d9
Taesu Hyeon <qvil1127@gmail.com>
2018-10-30 00:11:39 +0900

    #1.10 Note Route

--- src/Routes/Note/Note.js
+++ src/Routes/Note/Note.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { GET_NOTE } from "../../queries";
 
 export default class Note extends React.Component {
   render() {
-    return "hi";
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+
+    return (
+      <Query query={GET_NOTE} variables={{ id }}>
+        {({ data }) => {
+          console.log(data);
+          return null;
+        }}
+      </Query>
+    );
   }
 }
