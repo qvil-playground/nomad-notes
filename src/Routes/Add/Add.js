63281ad09e2bf34bd8e46770bf28a9f76c3d9c33
Taesu Hyeon <qvil1127@gmail.com>
2018-11-07 23:16:03 +0900

    #1-11 Add Note Route

--- src/Routes/Add/Add.js
+++ src/Routes/Add/Add.js
@@ -1,9 +1,39 @@
 import React from "react";
-import Editor from "../../components/Editor/Editor";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
+import Editor from "../../components/Editor";
+
+const ADD_NOTE = gql`
+  mutation createNote($title: String!, $content: String!) @client {
+    createNote(title: $title, content: $content) {
+      id
+    }
+  }
+`;
 
 export default class Add extends React.Component {
   render() {
     // return "hi";
-    return <Editor />;
+    return (
+      <Mutation mutation={ADD_NOTE}>
+        {createNote => {
+          this.createNote = createNote;
+          return <Editor onSave={this._onSave} />;
+        }}
+      </Mutation>
+    );
   }
+
+  _onSave = (title, content) => {
+    const {
+      history: { push }
+    } = this.props;
+
+    if (title !== "" && content !== "") {
+      this.createNote({ variables: { title, content } });
+      push("/");
+    }
+    
+    console.log(title, content);
+  };
 }
