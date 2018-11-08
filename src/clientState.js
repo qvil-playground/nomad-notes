75747bc5d5f913b444770c0c0a05f49271063bfc
Taesu Hyeon <qvil1127@gmail.com>
2018-11-09 01:52:06 +0900

    #1-13 Saving the Notes Offline

--- src/clientState.js
+++ src/clientState.js
@@ -1,16 +1,11 @@
 import { NOTE_FRAGMENT } from "./fragments";
 import { GET_NOTES } from "./queries";
+import { saveNotes } from "./offline";
 
 export const defaults = {
-  notes: [
-    {
-      __typename: "Note",
-      id: 1,
-      title: "First",
-      content: "Second"
-    }
-  ]
+  notes: []
 };
+
 export const typeDefs = [
   `
     schema {
@@ -58,7 +53,7 @@ export const resolvers = {
           notes: [newNote, ...notes]
         }
       });
-
+      saveNotes(cache);
       return newNote;
     },
     editNote: (_, { id, title, content }, { cache }) => {
@@ -78,6 +73,7 @@ export const resolvers = {
 
         data: updateNote
       });
+      saveNotes(cache);
       return updateNote;
     }
   }
